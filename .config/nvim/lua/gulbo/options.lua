d77eebda40bf8d2c0c67a407015748f9fe163e6a
gulbord <52690645+gulbord@users.noreply.github.com>
2024-03-24 19:06:27 +0100

    Change default tabstop to 2

--- .config/nvim/lua/gulbo/options.lua
+++ .config/nvim/lua/gulbo/options.lua
@@ -9,20 +9,20 @@ vim.opt.textwidth = 80
 vim.opt.splitbelow = true
 vim.opt.cursorline = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = -1
+vim.opt.shiftwidth = 0
+vim.opt.autoindent = true
 
 vim.opt.termguicolors = true
 
 vim.g.python3_host_prog = "/usr/bin/python3"
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "bib,bug,css,lua,markdown,r,rmd,stan,tex",
+  pattern = "c,cpp",
   callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
+    vim.opt_local.tabstop = 4
+    vim.opt_local.cindent = true
   end
 })
