946759dce745ef072d1b3450b8d0486b587a8505
gulbord <52690645+gulbord@users.noreply.github.com>
2024-06-05 14:00:54 +0200

    Add hardtime plugin, delete alabaster theme and lazy-load plugins by default

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,7 +22,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup(
+  "plugins",
+  {
+    defaults = { lazy = true },
+  }
+)
 
 -----------------
 -- COLORSCHEME --
