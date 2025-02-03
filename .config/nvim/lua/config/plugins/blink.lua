54964bfdcc70e08e97419be7b61755e6857a96ee
gulbord <52690645+gulbord@users.noreply.github.com>
2025-02-03 06:40:28 +0100

    Improve autocompletion with <C-l> accept and preselection

--- .config/nvim/lua/config/plugins/blink.lua
+++ .config/nvim/lua/config/plugins/blink.lua
@@ -5,12 +5,16 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "default",
+      ["<C-l>"] = { "accept" },
+    },
     appearance = {
       nerd_font_variant = "mono",
     },
     completion = {
       accept = { auto_brackets = { enabled = true } },
+      list = { selection = { preselect = true, auto_insert = true } },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 300,
